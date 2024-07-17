64d0c24e963504832e979936e6e4116fb80524b6
Edward Temple-Smithson <eddts@hotmail.com>
2024-07-17 11:29:25 +0100

    add no results component

---
+++ src/app/components/no-results/no-results.component.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "./no-results.module.css";
+
+export default async function NoResults({
+  onClear,
+  showClear,
+}: {
+  onClear: () => void;
+  showClear: boolean;
+}) {
+  return (
+    <div className={styles.noResults}>
+      <h3>No results</h3>
+      <p>{"Uh oh, looks like we don't have any holidays to show you."}</p>
+      {showClear && <button onClick={onClear}>Clear filters</button>}
+      <Link href="/">Restart booking</Link>
+    </div>
+  );
+}
