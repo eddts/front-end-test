0c1c3f57323ca3dc52127dc8aaa927e9a68bc328
Edward Temple-Smithson <eddts@hotmail.com>
2024-07-18 10:37:57 +0100

    last little bit of reorganisation to save a comment explaining it!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import "./globals.css";
+import "./app.scss";
 
 export const metadata: Metadata = {
   title: "Virgin Atlantic | Front End Coding Test",
